9e684c10ac424621245c5e4e04940fc5511fccc3
pluto-dev <plutonan@mailbox.org>
2024-01-31 00:50:39 +0100

    Create Category.cs

---
+++ src/PlutoCast.Desktop/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace PlutoCast.Desktop.Models;
+
+public class Category(int categoryId, string categoryName)
+{
+    public int CategoryId { get; set; } = categoryId;
+    public string CategoryName { get; set; } = categoryName;
+}
