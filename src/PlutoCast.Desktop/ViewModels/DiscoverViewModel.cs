d09d7d29881292ab0219392f6e9aa23aded23419
pluto-dev <plutonan@mailbox.org>
2024-02-04 22:19:52 +0100

    Exposing properties from BogusService in DiscoverViewModel.

--- src/PlutoCast.Desktop/ViewModels/DiscoverViewModel.cs
+++ src/PlutoCast.Desktop/ViewModels/DiscoverViewModel.cs
@@ -1,3 +1,28 @@
-﻿namespace PlutoCast.Desktop.ViewModels;
+﻿using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PlutoCast.Desktop.Models;
+using PlutoCast.Desktop.Services;
 
-public class DiscoverViewModel : BaseViewModel { }
+namespace PlutoCast.Desktop.ViewModels;
+
+[ObservableObject]
+public partial class DiscoverViewModel : BaseViewModel
+{
+    private readonly BogusService _bogusService;
+
+    public DiscoverViewModel(BogusService bogusService)
+    {
+        _bogusService = bogusService;
+    }
+
+    public List<TrendingPodcast> TopTrendingPodcasts => _bogusService.TopTrendingPodcasts;
+    public List<TrendingPodcast> NewsTrendingPodcasts => _bogusService.NewsTrendingPodcasts;
+    public List<TrendingPodcast> ComedyTrendingPodcasts => _bogusService.ComedyTrendingPodcasts;
+    public List<TrendingPodcast> ScienceTrendingPodcasts => _bogusService.ScienceTrendingPodcasts;
+    public List<TrendingPodcast> TrueCrimeTrendingPodcasts =>
+        _bogusService.TrueCrimeTrendingPodcasts;
+
+    public Dictionary<string, List<Category>> GroupedCategories => _bogusService.GroupedCategories;
+
+    public List<string> GroupedCategoryNames => _bogusService.GroupedCategoryNames;
+}
