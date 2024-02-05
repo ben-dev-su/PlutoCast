6ecd466dbf12b609e8d1560ecf0959b50b0abb06
pluto-dev <plutonan@mailbox.org>
2024-02-05 12:11:42 +0100

    Create ItemClickCommand dependecy property

--- src/PlutoCast.Desktop/Controls/CustomFlipView.cs
+++ src/PlutoCast.Desktop/Controls/CustomFlipView.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Windows.Input;
 using Microsoft.UI.Dispatching;
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 
@@ -9,6 +12,13 @@ public sealed class CustomFlipView : FlipView
 {
     private readonly DispatcherQueue _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
     private readonly DispatcherQueueTimer _timer;
+    public static readonly DependencyProperty ItemClickCommandProperty =
+        DependencyProperty.Register(
+            nameof(ItemClickCommand),
+            typeof(ICommand),
+            typeof(CustomFlipView),
+            new PropertyMetadata(default(ICommand))
+        );
 
     public CustomFlipView()
     {
@@ -18,7 +28,35 @@ public sealed class CustomFlipView : FlipView
         _timer.Tick += OnTick;
         PointerEntered += OnPointerEntered;
         PointerExited += OnPointerExited;
+        PointerPressed += OnPointerPressed;
         _timer.Start();
+    }
+
+    public ICommand? ItemClickCommand
+    {
+        get => (ICommand)GetValue(ItemClickCommandProperty);
+        set => SetValue(ItemClickCommandProperty, value);
+    }
+
+    private void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        if (e.Pointer.PointerDeviceType is not PointerDeviceType.Mouse)
+        {
+            return;
+        }
+
+        var currentPointProperties = e.GetCurrentPoint((UIElement)sender).Properties;
+        if (
+            currentPointProperties.PointerUpdateKind == PointerUpdateKind.RightButtonReleased
+            || currentPointProperties.IsRightButtonPressed
+        )
+        {
+            return;
+        }
+        if (ItemClickCommand is not null && ItemClickCommand.CanExecute(SelectedItem))
+        {
+            ItemClickCommand.Execute(SelectedItem);
+        }
     }
 
     private void OnPointerExited(object sender, PointerRoutedEventArgs e)
